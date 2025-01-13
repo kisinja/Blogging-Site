31af0b3ac1add949f3072a87574ce97040e6a9a6
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2025-01-13 14:26:12 +0300

    commit

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -33,9 +33,7 @@ const savePost = async (req, res) => {
         });
     }
 
-    setTimeout(() => {
-        res.status(200).json(isSaved ? "Post unsaved" : "Post saved");
-    }, 3000);
+    res.status(200).json(isSaved ? "Post unsaved" : "Post saved");
 };
 
 const getUserProfile = async (req, res) => {
